f43a08499ff9babe3ff797e4971205a002b371ea
kurudrive <kurudrive@gmail.com>
2014-11-07 15:04:50 +0900

    3PRの編集ボタンの追加と値をglobalから持ってくるように（dbを無駄に読みに行かないように）変更

--- module_topPR.php
+++ module_topPR.php
@@ -1,6 +1,6 @@
 <?php
-$options = biz_vektor_get_theme_options();
-if ( isset($options['top3PrDisplay']) && !$options['top3PrDisplay']) {
+global $biz_vektor_options;
+if ( isset($biz_vektor_options['top3PrDisplay']) && !$biz_vektor_options['top3PrDisplay']) {
 	// if Home Pr display
 
 	?>
@@ -8,58 +8,63 @@ if ( isset($options['top3PrDisplay']) && !$options['top3PrDisplay']) {
 	<div id="topPr">
 	<div id="topPrLeft" class="topPrOuter">
 	<div class="topPrInner">
-	<h3 class="topPrTit"><a href="<?php echo esc_url($options['pr1_link']) ?>"><?php echo $options['pr1_title'] ?></a></h3>
-	<?php if ($options['pr1_image']) { ?>
+	<h3 class="topPrTit"><a href="<?php echo esc_url($biz_vektor_options['pr1_link']) ?>"><?php echo $biz_vektor_options['pr1_title'] ?></a></h3>
+	<?php if ($biz_vektor_options['pr1_image']) { ?>
 		<div class="prImage">
-		<a href="<?php echo esc_url($options['pr1_link']) ?>">
-		<img src="<?php echo$options['pr1_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$options['pr1_title']) ; ?>" class="imageWide" />
-		<img src="<?php echo $options['pr1_image_s'] ?>" alt="" class="imageSmall" />
+		<a href="<?php echo esc_url($biz_vektor_options['pr1_link']) ?>">
+		<img src="<?php echo$biz_vektor_options['pr1_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$biz_vektor_options['pr1_title']) ; ?>" class="imageWide" />
+		<img src="<?php echo $biz_vektor_options['pr1_image_s'] ?>" alt="" class="imageSmall" />
 		</a>
 		</div>
 	<?php } ?>
 	<div class="topPrTxtBox">
-		<p class="topPrDescription"><a href="<?php echo esc_url($options['pr1_link']) ?>"><?php echo nl2br(esc_textarea($options['pr1_description'])) ?></a></p>
-		<div class="moreLink"><a href="<?php echo esc_url($options['pr1_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
+		<p class="topPrDescription"><a href="<?php echo esc_url($biz_vektor_options['pr1_link']) ?>"><?php echo nl2br(esc_textarea($biz_vektor_options['pr1_description'])) ?></a></p>
+		<div class="moreLink"><a href="<?php echo esc_url($biz_vektor_options['pr1_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
 	</div>
 	</div>
 	</div><!-- /#topPrLeft -->
-	
+
 	<div id="topPrCenter" class="topPrOuter">
 	<div class="topPrInner">
-	<h3 class="topPrTit"><a href="<?php echo esc_url($options['pr2_link']) ?>"><?php echo $options['pr2_title'] ?></a></h3>
-	<?php if ($options['pr2_image']) { ?>
+	<h3 class="topPrTit"><a href="<?php echo esc_url($biz_vektor_options['pr2_link']) ?>"><?php echo $biz_vektor_options['pr2_title'] ?></a></h3>
+	<?php if ($biz_vektor_options['pr2_image']) { ?>
 		<div class="prImage">
-		<a href="<?php echo esc_url($options['pr2_link']) ?>">
-		<img src="<?php echo $options['pr2_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$options['pr2_title']) ; ?>" class="imageWide" />
-		<img src="<?php echo $options['pr2_image_s'] ?>" alt="" class="imageSmall" />
+		<a href="<?php echo esc_url($biz_vektor_options['pr2_link']) ?>">
+		<img src="<?php echo $biz_vektor_options['pr2_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$biz_vektor_options['pr2_title']) ; ?>" class="imageWide" />
+		<img src="<?php echo $biz_vektor_options['pr2_image_s'] ?>" alt="" class="imageSmall" />
 		</a>
 		</div>
 	<?php } ?>
 	<div class="topPrTxtBox">
-		<p class="topPrDescription"><a href="<?php echo esc_url($options['pr2_link']) ?>"><?php echo nl2br(esc_textarea($options['pr2_description'])) ?></a></p>
-		<div class="moreLink"><a href="<?php echo esc_url($options['pr2_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
+		<p class="topPrDescription"><a href="<?php echo esc_url($biz_vektor_options['pr2_link']) ?>"><?php echo nl2br(esc_textarea($biz_vektor_options['pr2_description'])) ?></a></p>
+		<div class="moreLink"><a href="<?php echo esc_url($biz_vektor_options['pr2_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
 	</div>
 	</div>
 	</div><!-- /#topPrCenter -->
-	
+
 	<div id="topPrRight" class="topPrOuter">
 	<div class="topPrInner">
-	<h3 class="topPrTit"><a href="<?php echo esc_url($options['pr3_link']) ?>"><?php echo $options['pr3_title'] ?></a></h3>
-	<?php if ($options['pr3_image']) { ?>
+	<h3 class="topPrTit"><a href="<?php echo esc_url($biz_vektor_options['pr3_link']) ?>"><?php echo $biz_vektor_options['pr3_title'] ?></a></h3>
+	<?php if ($biz_vektor_options['pr3_image']) { ?>
 		<div class="prImage">
-		<a href="<?php echo esc_url($options['pr3_link']) ?>">
-		<img src="<?php echo $options['pr3_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$options['pr3_title']) ; ?>" class="imageWide" />
-		<img src="<?php echo $options['pr3_image_s'] ?>" alt="" class="imageSmall" />
+		<a href="<?php echo esc_url($biz_vektor_options['pr3_link']) ?>">
+		<img src="<?php echo $biz_vektor_options['pr3_image'] ?>" alt="<?php printf(__( 'Image of %s', 'biz-vektor' ),$biz_vektor_options['pr3_title']) ; ?>" class="imageWide" />
+		<img src="<?php echo $biz_vektor_options['pr3_image_s'] ?>" alt="" class="imageSmall" />
 		</a>
 		</div>
 	<?php } ?>
 	<div class="topPrTxtBox">
-		<p class="topPrDescription"><a href="<?php echo esc_url($options['pr3_link']) ?>"><?php echo nl2br(esc_textarea($options['pr3_description'])) ?></a></p>
-		<div class="moreLink"><a href="<?php echo esc_url($options['pr3_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
+		<p class="topPrDescription"><a href="<?php echo esc_url($biz_vektor_options['pr3_link']) ?>"><?php echo nl2br(esc_textarea($biz_vektor_options['pr3_description'])) ?></a></p>
+		<div class="moreLink"><a href="<?php echo esc_url($biz_vektor_options['pr3_link']) ?>"><?php echo _x( 'Read more', 'Link to page', 'biz-vektor' ); ?></a></div>
 	</div>
 	</div>
 	</div><!-- /#topPrRight -->
 	</div>
 	<!-- [ #topPr ] -->
 
+<?php } ?>
+<?php if ( is_user_logged_in() == TRUE && current_user_can('edit_theme_options') ) { ?>
+<div class="adminEdit">
+<a href="<?php echo site_url(); ?>/wp-admin/themes.php?page=theme_options#prBox" class="btn btnS btnAdmin"><?php _e('Edit', 'biz-vektor');?></a>
+</div>
 <?php } ?>
