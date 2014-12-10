9743a4c677e758f824ac305488cf6106393c1dd5
Shoji Imamura <imamura@vektor-inc.co.jp>
2014-12-10 20:20:45 +0900

    3prのＣＳＳ適用

--- module_topPR.php
+++ module_topPR.php
@@ -5,7 +5,7 @@ if ( isset($biz_vektor_options['top3PrDisplay']) && !$biz_vektor_options['top3Pr
 
 	?>
 	<!-- [ #topPr ] -->
-	<div id="topPr">
+	<div id="topPr" class="topPr">
 	<div id="topPrLeft" class="topPrOuter">
 	<div class="topPrInner">
 	<h3 class="topPrTit"><a href="<?php echo esc_url($biz_vektor_options['pr1_link']) ?>"><?php echo $biz_vektor_options['pr1_title'] ?></a></h3>
